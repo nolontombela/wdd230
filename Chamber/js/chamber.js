f937718ddeb82adb53d253eef07c8c3a4344fff0
bonolontombela <nto19001@byui.edu>
2022-05-28 01:04:33 +0200

    x

--- Chamber/js/chamber.js
+++ Chamber/js/chamber.js
@@ -1,5 +1,7 @@
 /* Current Date Header */
-const currentDateSpan = document.querySelector("currentDate")
+const currentDateSpan = document.querySelector("#currentDate");
+
+const now = new Date();
 
 currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
     dateStyle: "full",
@@ -7,10 +9,10 @@ currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   
   /* hamburger toggle menu */
   function toggleMenu() {
-      document.querySelector("#flexi-nav").classList.toggle("open");
+      document.querySelector("#flex-nav").classList.toggle("open");
       document.querySelector("#menuButton").classList.toggle("open");
   }
-  const x = document.querySelector("#menuButton");
+  const x = document.geElementById("#menuButton");
   x.onclick = toggleMenu;
   
   
@@ -21,7 +23,7 @@ currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   document.querySelector("#modDate").textContent = document.lastModified;
   
   /* Weekly Banner */
-  if(now.getDay() === 2) {
+  if(now.getDay() === 1) {
       document.getElementById("banner").style.display = "block";
   }
   const close = document.querySelector("#close");
