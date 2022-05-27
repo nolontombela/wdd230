cd1a11e0c7229edaee0d08831dacd9b5dce1ff2e
bonolontombela <nto19001@byui.edu>
2022-05-27 19:09:27 +0200

    updates

--- Chamber/js/chamber.js
+++ Chamber/js/chamber.js
@@ -20,7 +20,7 @@ currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   document.getElementById("currentYear").textContent = nd.getFullYear();
   document.querySelector("#modDate").textContent = document.lastModified;
   
-  /* Banner on Mondays & Tuesdays */
+  /* Weekly Banner */
   if(now.getDay() === 2) {
       document.getElementById("banner").style.display = "block";
   }
